31956723d17d64081a0c7faffb3f3e29e1ec70f4
Minh Quan Do <minhquan98vn@gmail.com>
2023-04-05 17:46:04 +0700

    feat: create Users and Projects model, write logic code to login and signup

--- server/app/app.js
+++ server/app/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
+const authRoute = require('./routes/auth.js');
+
+app.use(cors({
+  origin: '*'
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
 // middleware and routes
 app.get('/', (req, res) => {
   res.send('Hello World!');
-})
+});
+
+app.use('/auth', authRoute);
 
 module.exports = app;
