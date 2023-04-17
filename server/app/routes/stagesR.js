ff1d4dcd511cd43c27f50f7e6b65e5f74b495c92
tien231231 <tien231ub@gmail.com>
2023-04-17 17:56:48 +0700

    done

--- server/app/routes/stagesR.js
+++ server/app/routes/stagesR.js
@@ -9,7 +9,7 @@ const {addStage,searchnameStage} = require("../controllers/stages.js")
 router.post("/search",searchnameStage)
 
 router.post("/addstage",addStage)
-router.post("/addstage",addStage)
+
 
 
 module.exports = router;
