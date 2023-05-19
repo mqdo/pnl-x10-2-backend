c581d9b2df63f8e31430f5851760da40a7279df4
Minh Quan Do <minhquan98vn@gmail.com>
2023-05-19 12:07:19 +0700

    fix: adjust generateCode function to generate the right form of code

--- server/app/utils/generateCode.js
+++ server/app/utils/generateCode.js
@@ -1,6 +1,6 @@
 const { customAlphabet } = require('nanoid');
 
-function generateCode(type = 'prj') {
+function generateCode(type) {
   const nanoid = customAlphabet('1234567890abcdef', 10)
   const id = nanoid();
   return `${type}-${id}`;
